0605f7d5335c8bd3ee26e404bbcfad5c5cb00073
Newton Gain <newtongain7@gmail.com>
2023-11-10 00:38:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const app=  require('./app');
+const db = require('./config/db');
+
+const port = 3000;
+app.get('/',(req,res)=>{
+    res.send('Hello world')
+});
+app.listen(port,()=>{
+    console.log('Server Listening on Port http://localhost:3000');
+});
